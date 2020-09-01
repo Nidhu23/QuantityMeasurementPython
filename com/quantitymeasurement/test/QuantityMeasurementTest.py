395bdbbf0e3934916fe1151332cc9c77a1c32083
“Nidhu23” <nidhunanik@gmail.com>
2020-09-01 10:48:24 +0530

    [Nidhuna]Add: Test Case for Value check

--- com/quantitymeasurement/test/QuantityMeasurementTest.py
+++ com/quantitymeasurement/test/QuantityMeasurementTest.py
@@ -22,3 +22,15 @@ def test_givenZeroFeetAndFloatValue_WhenCompared_ShouldReturnFalse():
     first_feet = Feet(0.0)
     second_feet = float(0.0)
     assert first_feet != second_feet
+
+
+def test_givenOneFtAndOneFt_WhenCompared_ShouldReturnTrue():
+    first_feet = Feet(1.0)
+    second_feet = Feet(1.0)
+    assert first_feet == second_feet
+
+
+def test_givenOneFtAndTwoFt_WhenCompared_ShouldReturnFalse():
+    first_feet = Feet(1.0)
+    second_feet = Feet(2.0)
+    assert first_feet != second_feet
