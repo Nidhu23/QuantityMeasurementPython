f8f5b04ecb7ddf104fc9634826351704abb345aa
“Nidhu23” <nidhunanik@gmail.com>
2020-09-01 13:16:48 +0530

    [Nidhuna]Refactor: To Overcome DRY principle violation

--- com/quantitymeasurement/src/QuantityMeasurement.py
+++ com/quantitymeasurement/src/QuantityMeasurement.py
@@ -1,16 +1,8 @@
-class Feet:
-    def __init__(self, feet):
-        self.feet = feet
+class QuantityMeasurer:
+    def __init__(self, value):
+        self.value = value
 
     def __eq__(self, other):
-        if isinstance(other, Feet):
-            return self.feet == other.feet
+        if isinstance(other, QuantityMeasurer):
+            return self.value == other.value
         return False
-
-
-class Inch:
-    def __init__(self, inch):
-        self.inch = inch
-
-    def __eq__(self, other):
-        return self.inch == other
