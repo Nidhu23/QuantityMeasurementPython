19de695ea81e8406469f46e524f9d257e83132bc
“Nidhu23” <nidhunanik@gmail.com>
2020-09-01 12:38:30 +0530

    [Nidhuna]Add: New class Inch

--- com/quantitymeasurement/src/QuantityMeasurement.py
+++ com/quantitymeasurement/src/QuantityMeasurement.py
@@ -6,3 +6,11 @@ class Feet:
         if isinstance(other, Feet):
             return self.feet == other.feet
         return False
+
+
+class Inch:
+    def __init__(self, inch):
+        self.inch = inch
+
+    def __eq__(self, other):
+        return self.inch == other
