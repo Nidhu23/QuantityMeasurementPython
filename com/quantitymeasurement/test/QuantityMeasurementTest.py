ecaf31d225af9b43c0da5bb432120c3899646de3
“Nidhu23” <nidhunanik@gmail.com>
2020-09-01 10:10:19 +0530

    [Nidhuna]Add: Test Case for comparison of 0ft and None

--- com/quantitymeasurement/test/QuantityMeasurementTest.py
+++ com/quantitymeasurement/test/QuantityMeasurementTest.py
@@ -2,6 +2,11 @@ from com.quantitymeasurement.src.QuantityMeasurement import Feet
 
 
 def test_givenZeroFtAndZeroFt_WhenCompared_ShouldReturnTrue():
-    first_feet = Feet(0)
-    second_feet = Feet(0)
+    first_feet = Feet(0.0)
+    second_feet = Feet(0.0)
     assert first_feet == second_feet
+
+
+def test_givenZeroFtAndNull_WhenCompared_ShouldReturnFalse():
+    feet = Feet(0.0)
+    assert feet != None
