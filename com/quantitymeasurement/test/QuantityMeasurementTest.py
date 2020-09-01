d343de183776da69702b8051d309c9b9f2f37bbb
“Nidhu23” <nidhunanik@gmail.com>
2020-09-01 10:27:57 +0530

    [Nidhuna]Add: Test Case for reference check

--- com/quantitymeasurement/test/QuantityMeasurementTest.py
+++ com/quantitymeasurement/test/QuantityMeasurementTest.py
@@ -10,3 +10,9 @@ def test_givenZeroFtAndZeroFt_WhenCompared_ShouldReturnTrue():
 def test_givenZeroFtAndNull_WhenCompared_ShouldReturnFalse():
     feet = Feet(0.0)
     assert feet != None
+
+
+def test_TwoFeetInstanceVariable_WhenCompared_ShouldReturnTrue():
+    first_feet = Feet(0.0)
+    second_feet = first_feet
+    assert first_feet == second_feet
